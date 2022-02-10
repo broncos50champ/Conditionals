e8b966ed6327b09993062b242e0550225eda913e
Luis Segovia <luiissegovia@icloud.com>
2022-02-10 13:21:44 -0700

    Initial Commit

---
+++ Conditionals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conditionals
+//
+//  Created by Luis Segovia on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
